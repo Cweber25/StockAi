87cd94962fb2bf345aa2de39529de7720219850a
Cole Weber <Onesockweber@gmail.com>
2025-03-04 16:29:35 -0500

    for grace

--- mainStockPage.py
+++ mainStockPage.py
@@ -1,12 +1,12 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.config import Config
-
+# Turn cursor back on with sudo mv /usr/share/icons/PiXflat/cursors/left_ptr.bak /usr/share/icons/PiXflat/cursors/left_ptr
 Config.set('graphics', 'fullscreen', 'auto')
 
 class HelloWorldApp(App):
     def build(self):
-        return Label(text="Hello test", font_size='48sp')
+        return Label(text="Kill yourself", font_size='48sp')
 
 if __name__ == '__main__':
     HelloWorldApp().run()
